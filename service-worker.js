ef63924b21051c5c1345209a5ca5900c860ce5ed
doniyan <82924270+doniyan@users.noreply.github.com>
2025-09-29 22:37:15 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 //v2
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("absensi-cache").then(cache => {
+    caches.open("absensi-cache"-v2).then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -20,4 +20,5 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
